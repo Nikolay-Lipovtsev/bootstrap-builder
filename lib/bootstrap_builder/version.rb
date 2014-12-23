8d8dba6b2dfa460cdd63498ac782e775a31b285c
Nikolay Lipovtsev <n.lipovtsev@gmail.com>
2014-12-23 18:47:54 +0300

    First commit

---
+++ lib/bootstrap_builder/version.rb
@@ -0,0 +1,3 @@
+module BootstrapBuilder
+  VERSION = "0.0.1"
+end
---
+++ test/bootstrap_builder_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BootstrapBuilderTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, BootstrapBuilder
+  end
+end
